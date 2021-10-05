54f41db091ba4cb13e146edaa4d2b1dc89d13ab9
Ehsan Davari <70313192+iamehsandvr@users.noreply.github.com>
2021-10-05 16:30:37 +0330

    Update RoleDetails.plugin.js

--- RoleDetails.plugin.js
+++ RoleDetails.plugin.js
@@ -6,8 +6,8 @@
  * @invite xfvHwqXXKs
  * @authorLink https://instagram.com/ehsndvr
  * @website https://www.beheshtmarket.com
- * @source https://raw.githubusercontent.com/rauenzi/BetterDiscordAddons/master/Plugins/PermissionsViewer/PermissionsViewer.plugin.js
- * @updateUrl https://raw.githubusercontent.com/rauenzi/BetterDiscordAddons/master/Plugins/PermissionsViewer/PermissionsViewer.plugin.js
+ * @source https://github.com/iamehsandvr/RoleDetails.plugin/blob/e1c918f8107dd9c59a853a7f7421f9a493073c3b/RoleDetails.plugin.js
+ * @updateUrl https://github.com/iamehsandvr/RoleDetails.plugin/blob/e1c918f8107dd9c59a853a7f7421f9a493073c3b/RoleDetails.plugin.js
 */
 const config = {
     info: {
